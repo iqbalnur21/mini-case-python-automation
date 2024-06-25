43062bb02f6316f460bca482c16a9b0ab514812a
iqbalnur21 <m.iqbalnur21.min@gmail.com>
2024-06-25 23:12:07 +0700

    update token

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = "37c66099e80a20b02e776c1c18d7d7d7e07cf17719730f0dd59bc9275efe9885"
+token = "your_token"
 userAPI = "https://gorest.co.in/public/v2/users"
 url = "https://gorest.co.in/rest-console"
